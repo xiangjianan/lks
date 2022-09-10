111910a880cd3df4bf2660fb0142428dbd84d57b
xjn <xjn@xjndeMacBook-Air.local>
2022-09-10 19:18:10 +0800

    V2.0.0 点赞数排序

--- js/defer.js
+++ js/defer.js
@@ -45,114 +45,21 @@ $('#search').keydown((e) => {
     }
 });
 
-// 推荐网站
-let $href = $('#recipient-name');
-let $content = $('#message-text');
-new Vue({
-    el: '#send_msg',
-    data() {
-        return {
-            send_flag: true,
-        };
-    },
-    methods: {
-        send() {
-            let $send_msg = $('#send_msg');
-            let href = $href.val().trim();
-            let content = $content.val().trim();
-            let href_length = href.length;
-            let content_length = content.length;
-            this.send_flag = true;
-            if (!content && !href) {
-                $href.val('');
-                $content.val('');
-                this.ele_message('请输入链接、网站简介', 'info');
-            } else if (!href) {
-                $href.val('');
-                this.ele_message('请输入链接', 'info');
-            } else if (!content) {
-                $content.val('');
-                this.ele_message('请输入网站简介', 'info');
-            }
-            if (href_length > 256) {
-                this.ele_message('链接太长了，已超出256个字符的限制', 'warning');
-            } else if (content_length > 256) {
-                this.ele_message(`你一共写了${content_length}个字，已超出256个字符的限制`, 'warning');
-            }
-            let ip = '0.0.0.0';
-            if (this.send_flag) {
-                $send_msg.attr('disabled', true);
-                $send_msg.addClass('send_disabled');
-                $.ajax({
-                    type: "POST",
-                    dataType: "text",
-                    url: 'https://lks.helloxjn.com/api/web_reveive',
-                    data: {
-                        "href": href,
-                        "content": content,
-                        "ip": ip,
-                    },
-                    error: (res) => {
-                        $send_msg.attr('disabled', false);
-                        $send_msg.removeClass('send_disabled');
-                        if (res.status == 429) {
-                            this.$notify({
-                                title: '发送失败',
-                                message: `发送频率过高，请稍后再试！`,
-                                type: 'warning',
-                                center: true,
-                            });
-                        } else {
-                            this.$notify({
-                                title: '发送失败',
-                                message: `服务器在摸鱼！`,
-                                type: 'error',
-                                center: true,
-                            });
-                        }
-                    },
-                    success: (res) => {
-                        $send_msg.attr('disabled', false);
-                        $send_msg.removeClass('send_disabled');
-                        res = $.parseJSON(res);
-                        if (res.code == 1040) {
-                            this.$notify({
-                                title: '发送失败',
-                                message: `你的请求已被过滤 (￣^￣)ゞ`,
-                                type: 'error',
-                                center: true,
-                            });
-                        } else if (res.code == 1000) {
-                            $href.val('');
-                            $content.val('');
-                            this.$notify({
-                                title: '已发送',
-                                message: `感谢分享 (゜-゜)つロ`,
-                                type: 'success',
-                                center: true,
-                            });
-                        }
-                    }
-                });
-            }
-        },
-        ele_message(msg, type) {
-            this.send_flag = false;
-            $('.el-message__closeBtn').click();
-            this.$message({
-                message: msg,
-                type: type,
-                center: true,
-                offset: -1,
-                showClose: true,
-            });
-        },
-    }
-})
-$href.keydown((e) => {
-    if (e.keyCode === 13) {
-        $content.focus();
-        return false;
+// 热度排序
+localStorage.setItem('is_sort_like_num', 1);
+$('#scroll-to-hot').click(function name(params) {
+    if (localStorage.getItem('is_sort_like_num') == 1) {
+        $grid.isotope({ 
+            sortBy: 'like_num',
+            sortAscending: false, 
+        });
+        localStorage.setItem('is_sort_like_num', 0);
+    } else {
+        $grid.isotope({ 
+            sortBy: 'original-order',
+            sortAscending: true, 
+        });
+        localStorage.setItem('is_sort_like_num', 1);
     }
 });
 
